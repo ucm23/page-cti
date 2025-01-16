71c774fe0593b30e9fb3cf5d5425a3d630c4edf0
Ulises Cano Martinez <eziocano23@hotmail.com>
2025-01-16 11:38:28 -0600

    Add google analitics 16-01-2024

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,48 @@ import '../assets/motivation.styles.css';
 import '../assets/calls.styles.css';
 import '../assets/styles.css';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import * as gtag from '../lib/gtag';
+
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    const router = useRouter();
+
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url);
+      };
+      router.events.on('routeChangeComplete', handleRouteChange);
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      };
+    }, [router.events]);
+  
+    return (
+      <>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        <Component {...pageProps} />
+      </>
+    );
 }
 
 export default MyApp;
